8f47434d7c3951035990ef3659409b40f75e7894
Abdelhafid Hassou <88541319+SOBHEAK@users.noreply.github.com>
2023-05-23 14:38:17 +0100

    Hafid commit >> simple modifications

--- EnewsProject/EnewsApp/urls.py
+++ EnewsProject/EnewsApp/urls.py
@@ -1,9 +1,9 @@
-#from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     path('about.html', views.about, name='about'),
     path('contactus.html', views.contactus, name='contactus'),
